5ae7601f9ee5af8fdaf9006dd04b06e3bdae037c
Hubert Kasperek <hubertkasp13@gmail.com>
2024-04-20 20:49:00 +0200

    Release AI Companion V1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-full absolute bottom-0 flex flex-row  justify-center text-sm md:text-md xl:text-xl">
+        <footer className="w-full relative flex flex-row justify-center text-sm md:text-md xl:text-xl">
             <p><a className="hover:text-muted-foreground" href="https://github.com/Hukasx0/ai-companion">AI Companion v1</a> by <a href="https://github.com/Hukasx0/" className="text-primary hover:text-muted-foreground">Hubert "Hukasx0" Kasperek</a></p>
       </footer>
     )
